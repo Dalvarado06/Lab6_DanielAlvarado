5ae4fd18b5b7a41cc3804262f69caa6bf53a0f11
Daniel A. Alvarado <danielalvarado019@gmail.com>
2020-09-04 13:58:37 -0600

    Primer commit

---
+++ src/lab/pkg6_danielalvarado/Playlist.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab.pkg6_danielalvarado;
+import java.util.ArrayList;
+
+public class Playlist {
+    
+    private String nombre;
+    private ArrayList<Cancion> listaCanciones = new ArrayList();
+
+    public Playlist() {
+    }
+
+    public Playlist(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Cancion> getListaCanciones() {
+        return listaCanciones;
+    }
+
+    public void setListaCanciones(ArrayList<Cancion> listaCanciones) {
+        this.listaCanciones = listaCanciones;
+    }
+
+    @Override
+    public String toString() {
+        return "Playlist: "+ nombre;
+    }
+    
+    
+    
+}
